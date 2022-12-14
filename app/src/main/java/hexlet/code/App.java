41ebc04889aedfae63f614776339aa70ea338416
Konstantin Naumovich <konstantin000008@gmail.com>
2022-12-14 12:13:06 +0300

    first step of project

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+}
