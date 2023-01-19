7c95bbc7b15733cfc508c3587764528d3d685e5c
Konstantin Naumovich <konstantin000008@gmail.com>
2023-01-19 09:38:49 +0300

    second step of project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Brain Games!");
     }
 
 
