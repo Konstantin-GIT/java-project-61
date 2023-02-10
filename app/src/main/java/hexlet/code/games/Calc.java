f4776ebaaeffdcfa6fba8978f82b7532692ed958
Konstantin Naumovich <konstantin000008@gmail.com>
2023-02-10 15:10:44 +0300

    changed class Calc

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -6,14 +6,11 @@ import static hexlet.code.Engine.run;
 public class Calc  {
 
     private static final String GAME_DESCRIPTION = "What is the result of the expression?";
-    private static final int INDEX_OF_QUESTION = 0;
-    private static final int INDEX_OF_CORRECT_ANSWER = 1;
     private static final int MAX_NUMBER_TO_INTERVAL = 100;
-    private static final int SIZE_ARRAY_FOR_QUESTIONS_AND_ANSWERS = 2;
     private static final int NUMBER_OF_ROUND = 3;
 
     public static void startCalcGame() {
-        String[][] questionsAndAnswersOfGame = new String[NUMBER_OF_ROUND][SIZE_ARRAY_FOR_QUESTIONS_AND_ANSWERS];
+        String[][] questionsAndAnswersOfGame = new String[NUMBER_OF_ROUND][];
         for (int i = 0; i < questionsAndAnswersOfGame.length; i++) {
             questionsAndAnswersOfGame[i] = generateRoundData();
         }
@@ -21,16 +18,14 @@ public class Calc  {
     }
 
     private static String[] generateRoundData() {
-        String[] questionAndAnswerOfGame = new String[SIZE_ARRAY_FOR_QUESTIONS_AND_ANSWERS];
         Random random = new Random();
         String[] symbolSet = {"+", "-", "*"};
         String symbol = symbolSet[random.nextInt(symbolSet.length)];
         int number1 = random.nextInt(1, MAX_NUMBER_TO_INTERVAL);
         int number2 = random.nextInt(1, MAX_NUMBER_TO_INTERVAL);
-        questionAndAnswerOfGame[INDEX_OF_QUESTION] = number1 + " " + symbol + " " + number2;
-        String correctAnswer = Integer.toString(calculate(number1, number2, symbol));
-        questionAndAnswerOfGame[INDEX_OF_CORRECT_ANSWER] = correctAnswer;
-        return questionAndAnswerOfGame;
+        String question = number1 + " " + symbol + " " + number2;
+        String answer = Integer.toString(calculate(number1, number2, symbol));
+        return new String[] {question, answer};
     }
 
     private static int calculate(int number1, int number2, String arithmeticOperator) {
